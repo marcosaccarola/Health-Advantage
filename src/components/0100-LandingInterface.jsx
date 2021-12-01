8b8b27d54e26d5aa7e698a41087989904b98ec4b
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-01 12:03:16 +0100

    handle registration form

--- src/components/0100-LandingInterface.jsx
+++ src/components/0100-LandingInterface.jsx
@@ -1,11 +1,14 @@
-import { Card,Row,Col } from "react-bootstrap"
+import { useState } from "react"
+import { Card,Row,Col,Form } from "react-bootstrap"
 import bg from '../assets/dave-hoefler-unsplash-medium.jpg'
 import LoginRegisterForm from "./0110-LoginRegistrationForm"
 
 const LandingInterface=({setCurrentUser})=>{
+    const[showRegistration,setShowRegistration]=useState(false)
 
-    const openRegistrationModal=()=>{
-        console.log('HI')
+    const handleRegistrationComponent=()=>{
+        showRegistration===false?setShowRegistration(true):setShowRegistration(false)
+        console.log(showRegistration)
     }
 
     return(
@@ -15,12 +18,13 @@ const LandingInterface=({setCurrentUser})=>{
             <Card.Img variant="top" src={bg}/>
             <Card.Body>
                 <Card.Title>
-                <a className='text-light' onClick={openRegistrationModal}>
+                <a className='text-light' onClick={handleRegistrationComponent}>
                     <span className='text-warning'>Purple Cross</span> connects home healthcare professionals with patients, <span className='text-warning'>forever free</span>.
                     <br/>
                     <span className='text-warning'>Sign up in 45 seconds!</span>
                 </a>
                 </Card.Title>
+        {showRegistration===false?
                 <Card.Text>
                     <Row>
                         <Col xs={4}>value icon and text</Col>
@@ -33,9 +37,41 @@ const LandingInterface=({setCurrentUser})=>{
                         <Col xs={4}>value icon and text</Col>
                     </Row>
                 </Card.Text>
-                {/* <Button variant="primary">Go somewhere</Button> */}
+            :
+            <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlSelect1">
+                <Form.Label>Example select</Form.Label>
+                <Form.Control as="select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlSelect2">
+                <Form.Label>Example multiple select</Form.Label>
+                <Form.Control as="select" multiple>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Form>
+        }
             </Card.Body>
             </Card>
+            
         </div>
     )
 }
