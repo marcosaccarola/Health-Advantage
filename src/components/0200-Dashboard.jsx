0cb6d74941543f46ca70fe74b1e607539d2e9808
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-09 00:28:33 +0100

    user info component

--- src/components/0200-Dashboard.jsx
+++ src/components/0200-Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
-import { Card,Button, Image } from "react-bootstrap"
+import { Card } from "react-bootstrap"
+import UserInfoCard from "./0201-UserInfoCard"
 import InterventionsTakenInCharge from "./0210-InterventionsTakenInCharge"
 import ListOfRequests from "./0220-Requests"
 import PublishForm from "./0230-PublishForm"
@@ -23,88 +24,20 @@ const Dashboard=({currentUser,requests,setRequests})=>{
             <Card style={{ width: '80rem', borderWidth:8 }} 
                 className={currentUser.role==='practitioner'
                     ?'text-dark bg-light border-info'
-                    :'text-dark bg-warning'}
+                    :'text-dark bg-light border-warning'}
                     >
             <Card.Body>
-                <div className='row justify-content-between mx-5 mt-4 mb-5'>
-                    <Image src={currentUser.photo} 
-                        style={{width:100,height:100,objectFit:'cover',borderRadius:'50%'}}
-                        className='mr-5'
-                        />
-                    <div className='ml-5 text-muted'>
-                        <Card.Title className='row justify-content-start'>
-                            Practitioner
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            Role
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            Location
-                        </Card.Title>
-                    </div>
-                    <div className='ml-5 text-muted'>
-                        <Card.Title className='row justify-content-start'>
-                            :
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            : 
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            :
-                        </Card.Title>
-                    </div>
-                    <div className='ml-5 mr-5'>
-                        <Card.Title className='row justify-content-start'>
-                            {currentUser.firstName} {currentUser.lastName}
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            {currentUser.profession} 
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            {currentUser.zipcode}
-                        </Card.Title>
-                    </div>
-                    <div className='ml-5 text-muted'>
-                        <Card.Title className='row justify-content-start'>
-                            Education
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            Board
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            Specializations
-                        </Card.Title>
-                    </div>
-                    <div className='ml-5 text-muted'>
-                        <Card.Title className='row justify-content-start'>
-                            :
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            : 
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            :
-                        </Card.Title>
-                    </div>
-                    <div className='ml-5 mr-auto mr-5'>
-                        <Card.Title className='row justify-content-start'>
-                            {currentUser.firstName} {currentUser.lastName}
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            {currentUser.profession} 
-                        </Card.Title>
-                        <Card.Title className='row justify-content-start'>
-                            {currentUser.specializations}
-                        </Card.Title>
-                    </div>
-                </div>
+
+                <UserInfoCard currentUser={currentUser} />
+
+                <hr className='mb-5' />
                 <Card.Text>
                     {currentUser.role==='practitioner'
                     ?
                         (offeredServices.length===0)
                         ?
-                        <div className='row justify-content-start mx-5 mb-4 text-dark'>
-                            you have not yet offered your services, consult the list of service requests below and click reply
+                        <div className='row justify-content-start mx-5 mb-4 text-muted'>
+                            You have not yet offered your services, consult the list of service requests below and click reply
                         </div>
                         :
                         <InterventionsTakenInCharge offeredServices={offeredServices} currentUser={currentUser}/>
