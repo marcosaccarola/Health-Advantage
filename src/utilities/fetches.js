78c4f239018b9d96d991558d7cedea3e8250d2e2
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-09 01:02:10 +0100

    currentUser double fetch (practitioners and patients

--- src/utilities/fetches.js
+++ src/utilities/fetches.js
@@ -1,18 +1,37 @@
-const USERS_URL='http://localhost:3001/practitioner/login'
+const PRACTITIONERS_URL='http://localhost:3001/practitioner/login'
+const PATIENTS_URL='http://localhost:3001/patient/login'
 
 export const getUser=async({reqBody,setCurrentUser})=>{
     try {
-        const response=await fetch(USERS_URL,
+        const responsePractitioners=await fetch(PRACTITIONERS_URL,
             {
                 method:'POST',
                 body:JSON.stringify(reqBody),
                 headers:{'Content-Type':'application/json'}
             })
-            if(response.ok){
-                let user=await response.json()
+            if(responsePractitioners.ok){
+                let user=await responsePractitioners.json()
                 setCurrentUser(user)
             }else{
-                alert('User not found.')
+                getPatient({reqBody,setCurrentUser})
+            }
+    } catch (error) {
+        throw error
+    }
+}
+const getPatient=async({reqBody,setCurrentUser})=>{
+    try {
+        const responsePatient=await fetch(PATIENTS_URL,
+            {
+                method:'POST',
+                body:JSON.stringify(reqBody),
+                headers:{'Content-Type':'application/json'}
+            })
+            if(responsePatient.ok){
+                let user=await responsePatient.json()
+                setCurrentUser(user)
+            }else{
+                console.log('User not found')
             }
     } catch (error) {
         throw error
