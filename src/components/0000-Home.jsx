54a9dfe56a7878c00d54a6d5e45ab50066e3b271
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-12 19:02:28 +0100

    refactoring home page from bg

--- src/components/0000-Home.jsx
+++ src/components/0000-Home.jsx
@@ -2,36 +2,45 @@ import LandingInterface from "./0100-LandingInterface"
 import {useEffect, useState} from 'react'
 import Dashboard from "./0200-Dashboard"
 import {Button} from "react-bootstrap"
-import { getListOfInterventions } from "../utilities/fetches"
-// import requestsToIntervention from '../dbExample/requestsToIntervention.json'
+// import { getListOfInterventions } from "../utilities/fetches"
 
 const Home=()=>{
     const[currentUser,setCurrentUser]=useState()
     const[requests,setRequests]=useState()
     useEffect(() => {
-        // setRequests(requestsToIntervention)
-        getListOfInterventions({setRequests})
+        // getListOfInterventions({setRequests})
     }, [])
 
     return (
-        <div>
-            {currentUser&&
-                <div className='row justify-content-end mt-5'>
-                    <Button
-                        style={{borderWidth:4}}
-                        className={currentUser.role==='Practitioner'
-                            ?"border-info rounded-pill bg-light text-dark px-5"
-                            :"border-warning rounded-pill bg-light text-dark px-5"}
-                        onClick={()=>setCurrentUser()}
-                        >
-                        Logout
-                    </Button>
-                </div>
+        <div className='' style={{width: '100%', minHeight:'100vh', background:'#d7baad'}}>
+            <div 
+                style={{
+                    width:'16vh',minHeight:'8vh',background:'#16c2d5',marginTop:'8vh',marginLeft:'16vh'
+                    }}>
+                LOGO
+            </div>
+
+            
+            {currentUser
+            // &&
+    // ? CURRENT USER ? LOGOUT BUTTON : ...
+                // <div className='row justify-content-end mt-5'>
+                //     <Button
+                //         style={{borderWidth:4}}
+                //         className={currentUser.role==='Practitioner'
+                //             ?"border-info rounded-pill bg-light text-dark px-5"
+                //             :"border-warning rounded-pill bg-light text-dark px-5"}
+                //         onClick={()=>setCurrentUser()}
+                //         >
+                //         Logout
+                //     </Button>
+                // </div>
             }
 
             {currentUser
-                ?<Dashboard currentUser={currentUser} setCurrentUser={setCurrentUser} requests={requests} setRequests={setRequests} />
-                :<LandingInterface setCurrentUser={setCurrentUser} currentUser={currentUser} />
+    // ? CURRENT USER ? DASHBOARD : LANDIN INTERFACE
+                // ?<Dashboard currentUser={currentUser} setCurrentUser={setCurrentUser} requests={requests} setRequests={setRequests} />
+                // :<LandingInterface setCurrentUser={setCurrentUser} currentUser={currentUser} />
             }
         </div>
     )
