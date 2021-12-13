bbce4ab552b3d775151cd30a0fcba844809f0cd9
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-13 17:07:33 +0100

    debugging

--- src/components/0000-Home.jsx
+++ src/components/0000-Home.jsx
@@ -133,22 +133,22 @@ const Home=()=>{
                                                         style={{width:'100%',height:40,fontWeight:'lighter',fontSize:18,color:'#2e4450'}}>
                                                         <option>Choose an option</option>
                                                         <option value="1">Prevention of bedsores</option>
-                                                        <option value="3">Critical Patient Nutrition</option>
+                                                        <option value="2">Critical Patient Nutrition</option>
                                                         <option value="3">Infusion or injection therapy</option>
-                                                        <option value="3">Treatment of intravascular devices</option>
-                                                        <option value="3">Urostomy management</option>
-                                                        <option value="3">Tracheostomy management</option>
-                                                        <option value="3">Treatment of bladder catheters</option>
-                                                        <option value="3">Percutaneous Endoscopic Gastrostomy Management</option>
-                                                        <option value="3">Gastric tube management</option>
-                                                        <option value="2">Application of medicines for external use</option>
-                                                        <option value="3">Assistance or recovery from immobilization syndrome</option>
-                                                        <option value="3">Assistance related to incontinence or irregularities</option>
-                                                        <option value="3">Treats skin lesions (burns,neuropathic or vascular lesions)</option>
-                                                        <option value="3">Day assistance</option>
-                                                        <option value="3">Night assistance</option>
-                                                        <option value="3">24h assistance</option>
-                                                        <option value="3">Other</option>
+                                                        <option value="4">Treatment of intravascular devices</option>
+                                                        <option value="5">Urostomy management</option>
+                                                        <option value="6">Tracheostomy management</option>
+                                                        <option value="7">Treatment of bladder catheters</option>
+                                                        <option value="8">Percutaneous Endoscopic Gastrostomy Management</option>
+                                                        <option value="9">Gastric tube management</option>
+                                                        <option value="10">Application of medicines for external use</option>
+                                                        <option value="11">Assistance or recovery from immobilization syndrome</option>
+                                                        <option value="12">Assistance related to incontinence or irregularities</option>
+                                                        <option value="13">Treats skin lesions (burns,neuropathic or vascular lesions)</option>
+                                                        <option value="14">Day assistance</option>
+                                                        <option value="15">Night assistance</option>
+                                                        <option value="16">24h assistance</option>
+                                                        <option value="17">Other</option>
                                                     </Form.Select>
                                                 </Form.Text>
                                                 <Form.Text style={{fontWeight:'lighter',fontSize:18,color:'#2e4450'}}>
@@ -168,7 +168,7 @@ const Home=()=>{
                                                 <Form.Label
                                                     style={{fontWeight:'bold',fontSize:20,color:'#2e4450'}}
                                                     >Zip code</Form.Label>
-                                                <Form.Control type="email" placeholder="Enter email" />
+                                                <Form.Control type="email" placeholder="Enter zip code" />
                                                 <Form.Text style={{fontWeight:'lighter',fontSize:18,color:'#2e4450'}}>
                                                 Your address will not be shared.
                                                 </Form.Text>
@@ -177,7 +177,7 @@ const Home=()=>{
                                                 <Form.Label
                                                     style={{fontWeight:'bold',fontSize:20,color:'#2e4450'}}
                                                     >Name</Form.Label>
-                                                <Form.Control type="email" placeholder="Enter email" />
+                                                <Form.Control type="email" placeholder="Enter your name" />
                                                 <Form.Text style={{fontWeight:'lighter',fontSize:18,color:'#2e4450'}}>
                                                 Just your first name.
                                                 </Form.Text>
