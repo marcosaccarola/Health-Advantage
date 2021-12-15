c35b977933e7335bcd5d26dae06d242b6b6479f4
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-15 16:53:15 +0100

    dashboard navbar basic

--- src/components/1000-Dashboard.jsx
+++ src/components/1000-Dashboard.jsx
@@ -7,33 +7,19 @@ const NewDashboard=({currentUser,setCurrentUser})=>{
 
     return(
         <>
-            {/* <Navbar className='mt-5 navText'>
-                <Container>
-                    <Navbar.Brand href="#home">New request to intervention</Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                    <NavDropdown title="New Intervention" id="navbarScrollingDropdown" className='textColor'>
-                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action5">
-                            Something else here
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                    <Navbar.Text>
-                        Signed in as: <a href="#login">{currentUser.firstName}</a>
-                    </Navbar.Text>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar> */}
+            <Row className='green vh-100 justify-content-center'>
+                <Col sm={8} className='bg-light  my-5 rounded border border-dark'>
 
-            <Row className='nude vh-100 justify-content-center'>
-                <Col sm={8} className='pink my-5 rounded border border-white'>
-                    <Row className='bg-white'>
+                    <Row className='rounded-top'>
                         <Col sm={2} className='megaTitle'><GiElephant /></Col>
-                        <Col sm={8} className='megaTitle'></Col>
-                        <Col sm={2} className='megaTitle'><img src={currentUser.photo} className='avatar borderGreen' /></Col>
+                        <Col sm={8} className='megaTitle'>
+                            Patient card
+                        </Col>
+                        <Col sm={2} className='megaTitle'><img src={currentUser.photo} className='avatar' /></Col>
                     </Row>
+
+
+
                 </Col>
             </Row>
         </>
